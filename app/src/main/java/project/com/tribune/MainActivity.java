3d49b79cd8b930f0b333b401e7163a86b9ee0aba
DravitLochan <dlochangupta@gmail.com>
2017-01-12 02:06:01 +0530

    UI for facebook and google login

--- app/src/main/java/project/com/tribune/MainActivity.java
+++ app/src/main/java/project/com/tribune/MainActivity.java
@@ -1,13 +1,81 @@
 package project.com.tribune;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN=1;
+
+    private Context context;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private ChildEventListener mChildEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context=getApplicationContext();
+
+        mFirebaseDatabase=FirebaseDatabase.getInstance();
+        mFirebaseAuth=FirebaseAuth.getInstance();
+
+        mAuthStateListener=new FirebaseAuth.AuthStateListener()     //initialization of mAuthStateListener.
+        {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user= firebaseAuth.getCurrentUser();
+                if(user!=null)
+                {
+                    //// TODO: 12-01-2017 whichever activity will be decided 
+                }
+                else
+                {
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setProviders(
+                                            AuthUI.GOOGLE_PROVIDER,
+                                            AuthUI.FACEBOOK_PROVIDER
+                                    )
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        if(mAuthStateListener!=null)
+        {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 }
